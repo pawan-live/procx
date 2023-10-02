071cd3e9ff0b43799b40e7dbcf24e5b90ff5e46d
JaninduMunasinghe <munasin-ps20057@stu.kln.ac.lk>
2023-10-03 01:22:29 +0530

    added card to dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card"
+
+
 
 const page = () => {
-  return <div>Dashboard</div>;
+  return <div className="flex p-5" >
+  <Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+  </Card>
+</div>;
+
 };
 
 export default page;
