d4e46c94342cfff123bc0d932f79b3f7a9c216e6
[ThejMal01] <[thejani.mallawaarachchi@gmail.com]>
2023-10-11 04:02:38 +0530

    review orders folder structure created

---
+++ src/app/reviewOrders/procStaffReview/page.jsx
@@ -0,0 +1,46 @@
+//Janindu
+
+// import { Button } from "@/app/components/ui/button";
+// import {
+//   Table,
+//   TableBody,
+//   TableCaption,
+//   TableCell,
+//   TableHead,
+//   TableHeader,
+//   TableRow,
+// } from "@/app/components/ui/table";
+// import React from "react";
+
+// const page = () => {
+//   return (
+//     <div>
+//       <div>
+//         <search />
+//       </div>
+//       <Table>
+//         <TableCaption>A list of your recent invoices.</TableCaption>
+//         <TableHeader>
+//           <TableRow>
+//             <TableHead className="w-[100px]">Order ID</TableHead>
+//             <TableHead>Date</TableHead>
+//             <TableHead>Site</TableHead>
+//             <TableHead className="text-right">Action</TableHead>
+//           </TableRow>
+//         </TableHeader>
+//         <TableBody>
+//           <TableRow>
+//             <TableCell className="font-medium">INV001</TableCell>
+//             <TableCell>2023/10/10</TableCell>
+//             <TableCell>Colombo</TableCell>
+//             <TableCell className="text-right">
+//               <Button variant="destructive">Review</Button>
+//             </TableCell>
+//           </TableRow>
+//         </TableBody>
+//       </Table>
+//     </div>
+//   );
+// };
+
+// export default page;
