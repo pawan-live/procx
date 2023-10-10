7ddf46e425a3ed671b1e9ed04670eb31eb763ef6
[ThejMal01] <[thejani.mallawaarachchi@gmail.com]>
2023-10-11 04:31:41 +0530

    Proc staff order view ui draft

--- src/app/reviewOrders/procStaffReview/page.jsx
+++ src/app/reviewOrders/procStaffReview/page.jsx
@@ -1,46 +1,84 @@
-//Janindu
+"use client";
 
-// import { Button } from "@/app/components/ui/button";
-// import {
-//   Table,
-//   TableBody,
-//   TableCaption,
-//   TableCell,
-//   TableHead,
-//   TableHeader,
-//   TableRow,
-// } from "@/app/components/ui/table";
-// import React from "react";
+import { Button } from "@/app/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card";
+import { Checkbox } from "@/app/components/ui/checkbox";
+import { Label } from "@/app/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/app/components/ui/select";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/app/components/ui/table";
+import { Tabs, TabsContent } from "@/app/components/ui/tabs";
+import React from "react";
 
-// const page = () => {
-//   return (
-//     <div>
-//       <div>
-//         <search />
-//       </div>
-//       <Table>
-//         <TableCaption>A list of your recent invoices.</TableCaption>
-//         <TableHeader>
-//           <TableRow>
-//             <TableHead className="w-[100px]">Order ID</TableHead>
-//             <TableHead>Date</TableHead>
-//             <TableHead>Site</TableHead>
-//             <TableHead className="text-right">Action</TableHead>
-//           </TableRow>
-//         </TableHeader>
-//         <TableBody>
-//           <TableRow>
-//             <TableCell className="font-medium">INV001</TableCell>
-//             <TableCell>2023/10/10</TableCell>
-//             <TableCell>Colombo</TableCell>
-//             <TableCell className="text-right">
-//               <Button variant="destructive">Review</Button>
-//             </TableCell>
-//           </TableRow>
-//         </TableBody>
-//       </Table>
-//     </div>
-//   );
-// };
+const page = () => {
+  return (
+    <Tabs defaultValue="overview" className="space-y-4 p-5">
+      <TabsContent value="overview" className="space-y-4">
+        <div className="flex gap-6"></div>
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Review Orders</CardTitle>
+            <CardDescription>Review orders here.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableHead>Order ID</TableHead>
+                  <TableHead>Date</TableHead>
+                  <TableHead>Site Location</TableHead>
+                  <TableHead>Budget</TableHead>
+                  <TableHead>Catalogue Status</TableHead>
+                </TableRow>
+                <TableRow>
+                  <TableCell>001</TableCell>
+                  <TableCell>Jan 20, 2022</TableCell>
+                  <TableCell>Colombo</TableCell>
+                  <TableCell>200 000 LKR</TableCell>
+                  <TableCell>Restricted</TableCell>
+                  <TableCell>
+                    <Button variant="destructive">Review</Button>
+                  </TableCell>
+                </TableRow>
 
-// export default page;
+                <TableRow>
+                  <TableCell>002</TableCell>
+                  <TableCell>Jan 30, 2022</TableCell>
+                  <TableCell>Matara</TableCell>
+                  <TableCell>10 000 LKR</TableCell>
+                  <TableCell>Not Restricted</TableCell>
+                  <TableCell>
+                    <Button variant="destructive">Review</Button>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  );
+};
+
+export default page;
