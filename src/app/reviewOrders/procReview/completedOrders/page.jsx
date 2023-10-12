b659c56e03bd050abaca9f08c7561e8f5038fb05
[ThejMal01] <[thejani.mallawaarachchi@gmail.com]>
2023-10-12 16:40:57 +0530

    proc staff review order pages completed

--- src/app/reviewOrders/procReview/completedOrders/page.jsx
+++ src/app/reviewOrders/procReview/completedOrders/page.jsx
@@ -26,9 +26,9 @@ import React from "react";
 const Page = () => {
   const router = useRouter();
 
-  const handleReview = (e) => {
+  const handleCompletedReview = (e) => {
     e.preventDefault();
-    router.push("/reviewOrders/procReview/orderList/approveOrder");
+    router.push("/reviewOrders/procReview/completedOrders/viewCompletedOrder");
   };
 
   return (
@@ -37,10 +37,8 @@ const Page = () => {
         <div className="flex gap-6"></div>
         <Card className="w-full">
           <CardHeader>
-            <CardTitle>Review Orders</CardTitle>
-            <CardDescription>
-              Review orders sent by site managers here.
-            </CardDescription>
+            <CardTitle>Completed Orders</CardTitle>
+            <CardDescription>Review completed orders</CardDescription>
           </CardHeader>
           <CardContent>
             <Table>
@@ -49,6 +47,7 @@ const Page = () => {
                   <TableHead>Order ID</TableHead>
                   <TableHead>Order Date</TableHead>
                   <TableHead>Required Date</TableHead>
+                  <TableHead>Received Date</TableHead>
                   <TableHead>Site Location</TableHead>
                   <TableHead>Budget</TableHead>
                   <TableHead>Budget Status</TableHead>
@@ -58,12 +57,16 @@ const Page = () => {
                   <TableCell>OID001</TableCell>
                   <TableCell>Jan 20, 2022</TableCell>
                   <TableCell>Jan 30, 2022</TableCell>
+                  <TableCell>Jan 25, 2022</TableCell>
                   <TableCell>Colombo</TableCell>
                   <TableCell>200 000 LKR</TableCell>
                   <TableCell>Restricted</TableCell>
                   <TableCell>Restricted</TableCell>
                   <TableCell>
-                    <Button onClick={handleReview} variant="destructive">
+                    <Button
+                      onClick={handleCompletedReview}
+                      variant="destructive"
+                    >
                       Review
                     </Button>
                   </TableCell>
@@ -73,12 +76,16 @@ const Page = () => {
                   <TableCell>OID002</TableCell>
                   <TableCell>Jan 30, 2022</TableCell>
                   <TableCell>Feb 10, 2022</TableCell>
+                  <TableCell>Feb 05, 2022</TableCell>
                   <TableCell>Matara</TableCell>
                   <TableCell>10 000 LKR</TableCell>
                   <TableCell>Not Restricted</TableCell>
                   <TableCell>Restricted</TableCell>
                   <TableCell>
-                    <Button onClick={handleReview} variant="destructive">
+                    <Button
+                      onClick={handleCompletedReview}
+                      variant="destructive"
+                    >
                       Review
                     </Button>
                   </TableCell>
