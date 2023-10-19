fb979446e792e15a91dd97bca0e829ade2305003
[ThejMal01] <[thejani.mallawaarachchi@gmail.com]>
2023-10-19 15:51:18 +0530

    length issue

--- src/app/dashboard/reviewOrders/procReview/pendingOrders/[orderID]/page.jsx
+++ src/app/dashboard/reviewOrders/procReview/pendingOrders/[orderID]/page.jsx
@@ -26,28 +26,33 @@ import {
 import { Tabs, TabsContent } from "@/app/components/ui/tabs";
 import { API_URLS, BASE_LOCAL, BASE_URL } from "@/app/utils/constants";
 import axios from "axios";
+import { format, isValid, parseISO } from "date-fns";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import React from "react";
 
 // pass in params as props to access URL params
 const Page = ({ params }) => {
-  const router = useRouter();
-  const [pendingOrders, setPendingOrders] = React.useState([]);
-  const [orderId, setOrderId] = React.useState([]);
-  const [orderDate, setOrderDate] = React.useState([]);
-  const [requireDate, setRequireDate] = React.useState([]);
-  const [site, setSite] = React.useState([]);
-  const [budget, setBudget] = React.useState([]);
-  const [budgetStatus, setBudgetStatus] = React.useState([]);
-  const [restricted, setRestricted] = React.useState([]);
-  //add these fields
-  const [siteManagerName, setSiteManagerName] = React.useState([]);
-  const [supplier, setSupplier] = React.useState([]);
+  console.log(params);
+
+  const [order, setOrder] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const router = useRouter();
+
+  // const [pendingOrders, setPendingOrders] = React.useState([]);
+  // const [orderId, setOrderId] = React.useState([]);
+  // const [orderDate, setOrderDate] = React.useState([]);
+  // const [requireDate, setRequireDate] = React.useState([]);
+  // const [site, setSite] = React.useState([]);
+  // const [budget, setBudget] = React.useState([]);
+  // const [budgetStatus, setBudgetStatus] = React.useState([]);
+  // const [restricted, setRestricted] = React.useState([]);
+  // //add these fields
+  // const [siteManagerName, setSiteManagerName] = React.useState([]);
+  // const [supplier, setSupplier] = React.useState([]);
 
   useEffect(() => {
-    const getPendingOrderByID = async () => {
+    const getOrderByID = async () => {
       try {
         const res = await axios.get(
           `${BASE_LOCAL}${API_URLS.ORDERS}/${params.orderID}`,
@@ -55,21 +60,43 @@ const Page = ({ params }) => {
 
         console.log("SERVER response:", res);
         setIsLoading(false);
-        setOrderId(res.data.id);
-        setOrderDate(res.data.createdAt);
-        setRequireDate(res.data.deliverDate);
-        setSite(res.data.site);
-        setBudget(res.data.budget);
-        setBudgetStatus(res.data.budgetStatus);
-        setRestricted(res.data.restricted);
-        setSiteManagerName(res.data.siteManagerName);
-        setSupplier(res.data.supplier);
+        setOrder(res.data);
+        // setOrderId(res.data.id);
+        // setOrderDate(res.data.createdAt);
+        // setRequireDate(res.data.deliverDate);
+        // setSite(res.data.site);
+        // setBudget(res.data.budget);
+        // setBudgetStatus(res.data.budgetStatus);
+        // setRestricted(res.data.restricted);
+        // setSiteManagerName(res.data.siteManagerName);
+        // setSupplier(res.data.supplier);
       } catch (error) {
         console.error("ERROR fetching item:", error);
       }
     };
-    getPendingOrderByID();
+    getOrderByID();
   }, [params.orderID]);
+
+  //format date
+  function formatDate(date) {
+    return date && isValid(parseISO(date))
+      ? format(parseISO(date), "dd/MM/yyyy")
+      : null;
+  }
+  const orderDate = formatDate(order && order.createdAt);
+  const deliveryDate = formatDate(order && order.deliverDate);
+
+  //budget calculation
+  let totals = 0;
+  const budgetCal = (order) => {
+    let total = 0;
+    for (let i = 0; i < order.items.length; i++) {
+      total += order.items[i].price * order.items[i].qty;
+    }
+    total = totals;
+    totals = order.items.reduce((acc, item) => acc + item.price * item.qty, 0);
+    return totals;
+  };
 
   //get budget status
   const getBudgetStatus = (budget) => {
@@ -101,11 +128,11 @@ const Page = ({ params }) => {
               <div className="flex flex-col md:flex-row gap-x-4 gap-y-4 w-full">
                 <div className="flex flex-col space-y-1.5 w-full lg:w-1/2">
                   <Label>Order ID</Label>
-                  <Input type="text" defaultValue={orderId} disabled></Input>
+                  <Input type="text" defaultValue={order.id} disabled></Input>
                   <Label>Order raised by</Label>
                   <Input
                     type="text"
-                    defaultValue={siteManagerName}
+                    defaultValue={order.siteMgr}
                     disabled
                   ></Input>
                   {/* <Label>Description</Label>
@@ -113,11 +140,15 @@ const Page = ({ params }) => {
                 </div>
                 <div className="flex flex-col space-y-1.5 lg:w-1/2">
                   <Label>Supplier Name</Label>
-                  <Input type="text" defaultValue={supplier} disabled></Input>
+                  <Input
+                    type="text"
+                    defaultValue={order.supplier}
+                    disabled
+                  ></Input>
                   {/* <Label>Site Manager Phone</Label>
                   <Input type="text" defaultValue={orderId} disabled></Input> */}
                   <Label>Site Location</Label>
-                  <Input type="text" defaultValue={site} disabled></Input>
+                  <Input type="text" defaultValue={order.site} disabled></Input>
                 </div>
                 <div className="flex flex-col space-y-1.5 w-full lg:w-1/2">
                   <div className="flex flex-row  gap-x-4 justify-between">
@@ -133,7 +164,7 @@ const Page = ({ params }) => {
                       <Label>Required Date</Label>
                       <Input
                         type="text"
-                        defaultValue={requireDate}
+                        defaultValue={deliveryDate}
                         disabled
                       ></Input>
                     </div>
@@ -161,14 +192,18 @@ const Page = ({ params }) => {
             <div className="flex flex-col md:flex-row gap-x-4 gap-y-4 w-full">
               <div className="flex flex-col space-y-1.5 w-full lg:w-1/2">
                 <Label>Total Budget</Label>
-                <Input type="text" defaultValue={budget} disabled></Input>
+                <Input
+                  type="text"
+                  defaultValue={budgetCal(order)}
+                  disabled
+                ></Input>
               </div>
 
               <div className="flex flex-col space-y-1.5 w-full lg:w-1/2">
                 <Label>Budget Status</Label>
                 <Input
                   type="text"
-                  defaultValue={getBudgetStatus}
+                  defaultValue={getBudgetStatus(budgetCal(order))}
                   disabled
                 ></Input>
               </div>
@@ -201,31 +236,21 @@ const Page = ({ params }) => {
                   <TableHead>Quantity</TableHead>
                   <TableHead>Budget</TableHead>
                   <TableHead>Catalogue Status</TableHead>
-                  <TableHead>Selection</TableHead>
                 </TableRow>
-                <TableRow>
-                  <TableCell>001</TableCell>
-                  <TableCell>Gal</TableCell>
-                  <TableCell>150 LKR</TableCell>
-                  <TableCell>12</TableCell>
-                  <TableCell>1800</TableCell>
-                  <TableCell>Restricted</TableCell>
-                  <TableCell>
-                    <Checkbox />
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell>002</TableCell>
-                  <TableCell>Vali</TableCell>
-                  <TableCell>200 LKR</TableCell>
-                  <TableCell>200</TableCell>
-                  <TableCell>40000</TableCell>
-                  <TableCell>Not Restricted</TableCell>
-                  <TableCell>
-                    <Checkbox />
-                  </TableCell>
-                </TableRow>
+                {order &&
+                  order.items &&
+                  order.items.map((item) => (
+                    <TableRow key={item.id}>
+                      <TableCell>{item.id}</TableCell>
+                      <TableCell>{item.itemName}</TableCell>
+                      <TableCell>{item.price}</TableCell>
+                      <TableCell>{item.qty}</TableCell>
+                      <TableCell>{item.qty * item.price}</TableCell>
+                      <TableCell>
+                        {item.restricted ? "Restricted" : "Not Restricted"}
+                      </TableCell>
+                    </TableRow>
+                  ))}
               </TableBody>
             </Table>
           </CardContent>
